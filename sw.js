a24445593c110b1e5b2e2f09238dddc0e1d2fd44
Estoque 360 <almoxarifado.mgtecbombas@gmail.com>
2026-09-21 09:09:23 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,5 @@
-const CACHE = 'ferramentas-360-v1';
-const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
-
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url);
-  if (url.origin === self.location.origin && event.request.method === 'GET') {
-    event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
-  }
-});
+const CACHE_NAME = 'ferramentas-360-v4';
+const ASSETS = ['./','./index.html','./logo-360.png','./icon-192.png','./icon-512.png','./favicon.ico','./favicon-16.png','./favicon-32.png','./manifest.json'];
+self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())); });
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
+self.addEventListener('fetch', event => { if (event.request.method !== 'GET' || new URL(event.request.url).origin !== location.origin) return; event.respondWith(fetch(event.request).catch(() => caches.match(event.request))); });
